b3dcb125ad438483e3073432848bbcde10b9a1e8
Condensator <conder.ua@gmail.com>
2020-09-16 15:12:17 +0300

    Update test_file_jenkins.groovy

--- test_file_jenkins.groovy
+++ test_file_jenkins.groovy
@@ -2,33 +2,30 @@
 
 def String autopackage_dir = "package"
 def String version = "V.import.${env.BUILD_NUMBER}"
-def String schema = "schema_name"
 
-def String packageLoaderPath="http://localhost:9014"
+def String schema = "PRX_RS"
+def String packageLoaderPath="http://localhost:9014/Projects/12/packages"
 
 pipeline {
     agent any
     stages{
-        stage('Example') {
+        stage('Test') {
             steps{
                 script{
 		    def String package_source_dir = "${env.WORKSPACE}/scripts"
                     sh "printenv"
                     sh "chmod -R 777 ${autopackage_dir}"
                     dir (autopackage_dir) {
-                        //sh "find . -type f -printf \"%T@ %p\\n\" | sort -nr | cut -d\\  -f2-"
                         sh "echo version is ${version}"
                         sh "mkdir ${version}"
+			    
                         sh "mkdir ${version}/${schema}"
                         sh "mv ${package_source_dir}/* ${version}/${schema}/"
-
 			def scripts_list = findFiles(glob: "${version}/**/*")
-			    def scripts = []
+			def scripts = []
 			for (def file : scripts_list) {
  				scripts<<file.getName()
 			}
-
-				//sh (script: "find ${version} -type f -printf \"%f\\n\"", returnStdout: true).trim()
 			def json = [name: version, 
 				    operation: "create", 
 				    type: "regular", 
@@ -39,9 +36,9 @@ pipeline {
 			writeJSON(file: "${version}/package.json", json: json)
 			def zipFileName = "${version}.dbmpackage.zip"
 			zip(zipFile:"${zipFileName}", dir: "${version}")
-
 			sh "curl -F \"packageFile=@${zipFileName}\" -X POST ${packageLoaderPath}"
-	                    
+	                
+			sh "java -jar package command"
                         
                     }
                 }
