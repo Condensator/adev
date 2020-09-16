fc02caebbc447a157edcc4a4679c4479da83abca
Condensator <conder.ua@gmail.com>
2020-09-16 11:31:55 +0300

    Update test_file_jenkins.groovy

--- test_file_jenkins.groovy
+++ test_file_jenkins.groovy
@@ -1,10 +1,16 @@
+def String script_dir = "scripts"
+def String varsion = "V.import.${env.BUILD_NUMBER}"
+
 pipeline {
     agent any
     stages{
         stage('Example') {
             steps{
                 script{
-                    sh "find . -type f -printf \"%T@ %p\\n\" | sort -nr | cut -d\\  -f2-"
+                    sh "chmod -R 777 ${script_dir}"
+                    dir (script_dir) {
+                        sh "find . -type f -printf \"%T@ %p\\n\" | sort -nr | cut -d\\  -f2-"
+                    }
                 }
             }
         }
