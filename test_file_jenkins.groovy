f5377b5c09f379c4bc4f03d1af72a3c85b15aa04
Condensator <conder.ua@gmail.com>
2020-09-16 11:23:45 +0300

    Update test_file_jenkins.groovy

--- test_file_jenkins.groovy
+++ test_file_jenkins.groovy
@@ -1,21 +1,9 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            script{
-                sh "find . -type f -printf \"%T@ %p\\n\" | sort -nr | cut -d\\  -f2-"
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
+node {
+    stage('Example') {
+        if (env.BRANCH_NAME == 'master') {
+            echo 'I only execute on the master branch'
+        } else {
+            echo 'I execute elsewhere'
         }
     }
 }
