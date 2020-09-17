ad49d2dfa580afcff14b2d35b40c28f508aa4306
Condensator <conder.ua@gmail.com>
2020-09-17 09:44:14 +0300

    Update test_file_jenkins.groovy

--- test_file_jenkins.groovy
+++ test_file_jenkins.groovy
@@ -8,7 +8,7 @@ def user_name = "admin@dbmaestro.com"
 def auth_token = "123456"
 def dbm_address = "192.168.99.1:8017"
 def package_source_dir = "scripts"
-def autopackage_dir = "Jenkinsfiles_DB"
+def autopackage_dir = "package"
 def String version = "V.import.${env.BUILD_NUMBER}"
 
 def String schema = "liq2"
